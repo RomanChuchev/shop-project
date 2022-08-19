496b2920f28432d46e84d6221764065e56b2e09e
Roman Chuchev <romanok20@gmail.com>
2022-08-19 14:26:32 +0300

    Fix close btn

--- src/components/AlertBye/AlertBye.jsx
+++ src/components/AlertBye/AlertBye.jsx
@@ -1,21 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import "./alertBye.css";
 
 function AlertBye({ name = "", closeAlertBye = Function.prototype }) {
-  const [classes, setClasses] = useState(
-    "alert alert-dismissible alert-success alert-bye-position text-light"
-  );
-
-  const close = () => {
-    setClasses(
-      "alert alert-dismissible alert-success alert-bye-position text-light hidden"
-    );
-  };
-
   useEffect(() => {
-    setClasses(
-      "alert alert-dismissible bg-success alert-bye-position text-black"
-    );
     const timerId = setTimeout(() => closeAlertBye(), 3000);
 
     return () => {
@@ -24,7 +11,7 @@ function AlertBye({ name = "", closeAlertBye = Function.prototype }) {
   }, [name, closeAlertBye]);
 
   return (
-    <div className={classes}>
+    <div className="alert alert-dismissible alert-success alert-bye-position text-light">
       <font style={{ verticalAlign: "inherit" }}>
         <font style={{ verticalAlign: "inherit" }}>
           <strong>Товары успешно оплачены!</strong>{" "}
